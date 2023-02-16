4be8e3e461eb3dbb2b606af2928e06e09fded098
shadab <shadab14meb346@gmail.com>
2023-02-16 14:32:49 +0530

    fix failing test case

--- types/user.ts
+++ types/user.ts
@@ -2,10 +2,12 @@ export interface IUser {
   id?: number;
   email?: string;
   admin?: boolean;
+  name?: string;
 }
 
 export type UserForJWTGeneration = {
   id: number;
   email: string;
   admin: boolean;
+  name: string;
 };
