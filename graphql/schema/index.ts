9e2669c05cb00721aa3a19757dab9474b955eb11
shadab <shadab14meb346@gmail.com>
2023-03-04 01:20:15 +0530

    remove the unrequired queries

--- graphql/schema/index.ts
+++ graphql/schema/index.ts
@@ -1,6 +1,5 @@
 import DataSourceTypeDef from "./data-source";
 import healthCheckTypeDef from "./health-check";
-import hubSpotTypeDef from "./hubspot";
 import queryTypeDef from "./query";
 import userTypeDef from "./user";
 import workspaceTypeDef from "./workspace";
@@ -9,7 +8,6 @@ const typeDefs = [
   userTypeDef,
   healthCheckTypeDef,
   DataSourceTypeDef,
-  hubSpotTypeDef,
   workspaceTypeDef,
   queryTypeDef,
 ];
