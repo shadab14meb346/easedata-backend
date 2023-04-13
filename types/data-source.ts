d85a5654c4fffba40ca80abcd17793d2014a303c
shadab <shadab14meb346@gmail.com>
2023-04-13 17:47:16 +0530

    add oauth-callback for msoffice

--- types/data-source.ts
+++ types/data-source.ts
@@ -1,6 +1,7 @@
 export enum DataSourceType {
   HUB_SPOT = "HUB_SPOT",
   GOOGLE_ANALYTICS = "GOOGLE_ANALYTICS",
+  MS_OFFICE_SHEET = "MS_OFFICE_SHEET",
 }
 export enum HUB_SPOT_TABLES {
   CONTACTS = "contacts",
