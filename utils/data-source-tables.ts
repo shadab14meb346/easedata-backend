63c6c227c0806ae5ae9dde498ab7cf667510441f
Shadab <shadab14meb346@gmail.com>
2023-04-17 20:02:59 +0530

    update logic the oauthcallback for gsheet as datasource (#29)

--- utils/data-source-tables.ts
+++ utils/data-source-tables.ts
@@ -17,8 +17,10 @@ const hubSpotTables = [
 const dataSourceTablesMap = new Map([[DataSourceType.HUB_SPOT, hubSpotTables]]);
 export const getDataSourceTables = (dataSourceType: DataSourceType) => {
   if (
+    //TODO:improve the logic so that when we add more data sources for which tables are not, we don't have to add them here.
     dataSourceType === DataSourceType.GOOGLE_ANALYTICS ||
-    dataSourceType === DataSourceType.MS_OFFICE_SHEET
+    dataSourceType === DataSourceType.MS_OFFICE_SHEET ||
+    dataSourceType === DataSourceType.GSHEET
   ) {
     return [];
   }
