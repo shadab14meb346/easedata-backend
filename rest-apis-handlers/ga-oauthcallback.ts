55464c2ec84329cee63a3b38c4f4af73929e2801
Shadab <shadab14meb346@gmail.com>
2023-04-17 21:30:57 +0530

    bugfix of the data for deals object (#30)
    * bugfix of the data for deals object
    
    * fix the lambada `ga-oauthcallback` being size more then 250 mb

--- rest-apis-handlers/ga-oauthcallback.ts
+++ rest-apis-handlers/ga-oauthcallback.ts
@@ -1,26 +1,33 @@
-import { google } from "googleapis";
-
 import { verifyToken } from "../common/jwt";
 import { DataSource } from "../model/data-source";
 import { DataSourceType } from "./../types/data-source";
 import { addDataSourceToWorkspace } from "../model/workspace-to-data-source";
+import querystring from "querystring";
+import axios from "axios";
 
-const oauth2Client = new google.auth.OAuth2(
-  process.env.GA_CLIENT_ID,
-  process.env.GA_CLIENT_SECRET,
-  process.env.GA_REDIRECT_URI
-);
 export async function handler(event, context, callback) {
   console.log({ qs: event?.queryStringParameters });
   const code = event?.queryStringParameters?.code;
   const state = JSON.parse(event.queryStringParameters.state);
   const user = verifyToken(state.token);
   //TODO: validate the workspaceId is valid and the "user" has access to it
+  const requestBody = {
+    code,
+    client_id: process.env.GA_CLIENT_ID,
+    client_secret: process.env.GA_CLIENT_SECRET,
+    redirect_uri: process.env.GA_REDIRECT_URI,
+    grant_type: "authorization_code",
+  };
+  const tokenEndpoint = "https://oauth2.googleapis.com/token";
 
-  const { tokens } = await oauth2Client.getToken(code);
+  const response = await axios.post(
+    tokenEndpoint,
+    querystring.stringify(requestBody)
+  );
+  const { access_token, refresh_token } = response.data;
   const dataSourceRow = await DataSource.create({
-    access_token: tokens.access_token as string,
-    refresh_token: tokens.refresh_token as string,
+    access_token: access_token as string,
+    refresh_token: refresh_token as string,
     type: DataSourceType.GSHEET,
   });
   console.log({ dataSourceRow });
